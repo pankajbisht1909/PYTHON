a7cd500dc2477db92cd5ebe6b3568ac2bdbddc5e
Pankaj Bisht <130260590+pankajbisht1909@users.noreply.github.com>
2023-09-11 06:54:22 +0530

    Add files via upload

---
+++ mathematics operations.py
@@ -0,0 +1,15 @@
+#TO FIND THE MEAN
+import statistics
+data=[2,4,20,8,10]
+print("The mean of the data is:",statistics.mean(data))
+
+#TO FIND THE MEDIAN
+print("The median of the data is:",statistics.median(data))
+
+#TO FIND THE VARIANCE
+print("The median of the data is:",statistics.variance(data))
+
+#TO FIND THE STANDARD DEVIATION
+print("The median of the data is:",statistics.stdev(data))
+
+
